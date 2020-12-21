8b7e36cfe81fb258abe45ca62107c0f14ecfe78b
bsixel <sixelbenjamin@gmail.com>
2020-12-20 19:04:54 -0600

    Removed .gradle folder from git, autogenerated mod versions

--- src/main/java/com/bsixel/packutils/ModInfo.java
+++ src/main/java/com/bsixel/packutils/ModInfo.java
@@ -1,8 +1,8 @@
 package com.bsixel.packutils;
 
-final public class ModInfo {
+final public class ModInfo { // Don't update me manually!
     public static final String MODID = "etheralt-pack-utils";
     public static final String NAME = "Etheralt Pack Utils";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "AUTO_VERSION";
     public static final String ACCEPTED_MINECRAFT = "[1.12,1.12.2]";
 }
